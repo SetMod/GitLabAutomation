334d6eadebc3955c8e9d49169cbb0d43531b2045
SetMod <voroncka.andrey@gmail.com>
2023-10-22 18:51:18 +0300

    Update app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app_port = int(os.getenv("APP_HOST", "0.0.0.0"))
+app_host = os.getenv("APP_PORT", 5000)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host=app_host, port=app_port)
